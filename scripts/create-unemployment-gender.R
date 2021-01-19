85fd486840b744ca1b72fa02fc411bc74c76140c
Vusani3 <vusanimasocha@gmail.com>
2021-01-19 13:02:53 +0200

    Unemployment by gender  script

---
+++ scripts/create-unemployment-gender.R
@@ -0,0 +1,7 @@
+library(dplyr)
+library(readr)
+
+unemployment_gender <- read_delim(file.path("data-raw", "unemployment_gender.csv"),
+                                ";")
+
+usethis::use_data(unemployment_gender, overwrite = TRUE)
