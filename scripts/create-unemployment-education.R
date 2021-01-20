786ae4d004198036d54d0b83d51d17fbdd19f932
Vusani3 <vusanimasocha@gmail.com>
2021-01-20 15:53:55 +0200

    Adding line breaks

--- scripts/create-unemployment-education.R
+++ scripts/create-unemployment-education.R
@@ -1,7 +1,9 @@
 library(dplyr)
 library(readr)
 
-unemployment_education <- read_delim(file.path("data-raw", "unemployment_education.csv"),
+unemployment_education <- read_delim(file.path(
+  "data-raw",
+  "unemployment_education.csv"),
                                 ";")
 
 usethis::use_data(unemployment_education, overwrite = TRUE)
--- scripts/create-unemployment-gender.R
+++ scripts/create-unemployment-gender.R
@@ -1,7 +1,8 @@
 library(dplyr)
 library(readr)
 
-unemployment_gender <- read_delim(file.path("data-raw", "unemployment_gender.csv"),
+unemployment_gender <- read_delim(file.path("data-raw",
+                                            "unemployment_gender.csv"),
                                 ";")
 
 usethis::use_data(unemployment_gender, overwrite = TRUE)
