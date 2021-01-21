bda631c0585efacdaa3513c677c25e1a926df37f
Vusani3 <vusanimasocha@gmail.com>
2021-01-21 08:43:35 +0200

    Correcting alignment

--- scripts/create-unemployment-gender.R
+++ scripts/create-unemployment-gender.R
@@ -1,8 +1,13 @@
 library(dplyr)
 library(readr)
 
-unemployment_gender <- read_delim(file.path("data-raw",
-                                            "unemployment_gender.csv"),
-                                ";")
+unemployment_gender <- read_delim(
+  file.path(
+    "data-raw",
+    "unemployment_gender.csv"
+    ),
+  ";",
+  skip = 2,
+  )
 
 usethis::use_data(unemployment_gender, overwrite = TRUE)
